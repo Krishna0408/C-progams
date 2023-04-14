31cb7590717f60ed4e8363786465ae4691e0f74d
Krishna Prasath <98581299+Krishna0408@users.noreply.github.com>
2023-04-14 11:57:38 +0530

    largest Palindrome in C

---
+++ largestPalindrome.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{
+    char a[10];
+    scanf("%s",a);
+    int n=strlen(a);
+    int len=(n*2)+1;
+    char b[len];
+    int c[len],d=0,y;
+    int j=0;
+    for(int i=0;i<len;i++)
+    {
+       b[j]='#';
+       b[++j]=a[i];
+       ++j;
+    }
+    // for(int i=0;i<len;i++)
+    // {
+    //    printf("%c",b[i]); 
+    // }
+    for(int i=0;i<len;i++)
+    {
+        int count=0;
+        int x=i;
+    for(int k=i+1;k<len;k++)
+    {
+      --x;
+     if(b[x]!=b[k])
+     break;
+     else
+     count++;
+    }
+    c[d++]=count;
+       
+    }
+//   for(int i=0;i<len;i++)
+//   {
+//       printf("%d ",c[i]);
+//   }
+  int max=c[0];
+ for(int i=0;i<len;i++)
+ {
+     if(max<c[i])
+     {
+         max=c[i];
+          y=i;
+     }
+ }
+//  printf("\n%d ",max);
+//  printf("\n%d ",y);
+ printf("\n");
+for(int i=y-max;i<=max+y;i++)
+{
+    if(b[i]!='#')
+    {
+        printf("%c",b[i]);
+    }
+}
+}
